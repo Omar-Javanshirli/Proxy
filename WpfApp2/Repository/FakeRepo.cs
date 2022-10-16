88f77a033046dc7c0f36ac42be9adda8541dc7d7
MirTalib Huseynzade <mirtalibhuseynzade@gmail.com>
2022-10-16 21:48:21 +0400

    Add project files.

---
+++ WpfApp2/Repository/FakeRepo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Repository
+{
+    
+    interface ICache
+    {
+        List<string> GetValue(string key);
+    }
+
+    public class RemoteDataBase : ICache
+    {
+        private List<string> dataBase;
+
+        public RemoteDataBase()
+        {
+            dataBase = new List<string>
+            {
+                "alma",
+                "armud",
+                "adam",
+                "heyvan"
+            };
+        }
+
+        public List<string> GetValue(string key)
+        {
+            var temp = new List<string>();
+            foreach (var item in dataBase)
+            {
+                if (item.StartsWith(key))
+                {
+                    temp.Add(item);
+                }
+            }
+            if (temp != null)
+            {
+                return temp;
+            }
+            return null;
+        }
+    }
+
+    public class CasheProxy : ICache
+    {
+        private List<string> CasheDataBase;
+        private RemoteDataBase dataBase;
+
+        public CasheProxy()
+        {
+            CasheDataBase=new List<string>();
+            CasheDataBase.Add("nar");
+            dataBase=new RemoteDataBase();
+        }
+
+        public List<string> GetValue(string key)
+        {
+            var temp=new List<string>();
+            foreach (var item in CasheDataBase)
+            {
+                if (item.StartsWith(key))
+                {
+                    temp.Add(item);
+                }
+                else
+                {
+                    var data=dataBase.GetValue(key); 
+                    if (data != null)
+                    {
+                        foreach (var d in data)
+                        { 
+                            CasheDataBase.Add(d);
+                            temp.Add(d);
+                        }
+                        return temp;
+                    }
+                }
+                return temp;
+            } 
+            return null;
+
+        }
+    }
+}
+
+
---
+++ WpfApp2/ViewModel/AppViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.Command;
+using WpfApp2.Repository;
+
+namespace WpfApp2.ViewModel
+{
+    public class AppViewModel:BaseViewModel
+    {
+        public RelayCommand TextChangeCommand { get; set; }
+        private string data;
+
+        public string Data
+        {
+            get { return data; }
+            set { data = value; OnPropertyChanged(); }
+        }
+
+        private List<string> getData;
+
+        public List<string> GetData
+        {
+            get { return getData; }
+            set { getData = value; OnPropertyChanged(); }
+        }
+
+        public AppViewModel()
+        {
+            TextChangeCommand = new RelayCommand((e) =>
+              {
+                  ICache cache = new CasheProxy();
+                  GetData =cache.GetValue(Data);
+              });
+        }
+    }
+}
